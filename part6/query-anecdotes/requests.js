1d46eab059a83e9836c2ea1706b2fb78e14002fc
okkonie <okko.niemikorpi@gmail.com>
2026-06-05 15:27:27 +0300

    6.16-6.19

---
+++ part6/query-anecdotes/requests.js
@@ -0,0 +1,39 @@
+const baseUrl = 'http://localhost:3001/anecdotes'
+
+const getId = () => (100000 * Math.random()).toFixed(0)
+
+export const getAnecdotes = async () => {
+  const res = await fetch(baseUrl)
+  if (!res.ok) {
+    throw new Error('Failed to fetch notes')
+  }
+  return await res.json()
+}
+
+export const createAnecdote = async (anecdote) => {
+  const res = await fetch(baseUrl, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ content: anecdote, id: getId(), votes: 0 })
+  })
+
+  if (!res.ok) {
+    throw new Error('Failed to create note')
+  }
+
+  return await res.json()
+}
+
+export const updateAnecdote = async (anecdote) => {
+  const res = await fetch(`${baseUrl}/${anecdote.id}`, {
+    method: 'PUT',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(anecdote)
+  })
+
+  if (!res.ok) {
+    throw new Error('Failed to update note')
+  }
+
+  return await res.json()
+}
